5fe1358a1939e7e793529f1ef65c4452d0aa19c8
rhayes13 <rhayes13@emich.edu>
2020-11-06 23:20:26 -0500

    subtotal, delete, etc.

--- bbb/shopping_cart.php
+++ bbb/shopping_cart.php
@@ -2,6 +2,9 @@
 <?php
 	include_once 'connect_to_database.php';
 ?>
+<!--
+	2. Quantity is not being updated (currently hardcoded)
+-->
 
 <head>
 	<title>Shopping Cart</title>
@@ -10,16 +13,20 @@
 	function del(isbn){
 		window.location.href="shopping_cart.php?delIsbn="+ isbn;
 	}
-	//TODO
+
+	//2. TODO
 	function url(sale_id, quantity, isbn){
+		console.log(sale_id);
+		console.log(isbn);
+		console.log(quantity);
+		//var q = document.getElementById("sale_id").value;
+		// console.log(q);
 		window.location.href="shopping_cart.php?sale_id="+ sale_id + "&quantity=" + quantity + "&isbn=" + isbn;
-		alert("hi");
+		// alert("hi");
 	}
-</script>
 	</script>
 </head>
 <body>
-
 
 	<table align="center" style="border:2px solid blue;">
 		<tr>
@@ -44,79 +51,98 @@
 			<td  colspan="3">
 				<div id="bookdetails" style="overflow:scroll;height:180px;width:400px;border:1px solid black;">
 					<table align="center" BORDER="2" CELLPADDING="2" CELLSPACING="2" WIDTH="100%">
-						<th width='10%'>Remove</th><th width='60%'>Book Description</th><th width='10%'>Qty</th><th width='10%'>Price</th>
-						<tr><td><button name='delete' id='delete' onClick='del("123441");return false;'>Delete Item</button></td><td>iuhdf</br><b>By</b> Avi Silberschatz</br><b>Publisher:</b> McGraw-Hill</td><td><input id='txt123441' name='txt123441' value='1' size='1' /></td><td>12.99</td></tr>					</table>
+						<th width='10%'>Remove</th>
+						<th width='60%'>Book Description</th>
+						<th width='10%'>Qty</th>
+						<th width='10%'>Price</th>
 
 						<?php
-							//TODO
+							//Update quantity in db
 							function urlFun($conn, $sale_id, $quantity, $isbn) {
-								$SQL = "UPDATE SALE
-										SET quantity=\".$quantity.\"
-										WHERE sale_id=\".$sale_id.\" AND ISBN=\".$isbn.\"";
+								$quantity_query = "UPDATE SALE
+										SET quantity='$quantity'
+										WHERE sale_id='$sale_id' AND ISBN='$isbn'";
+								mysqli_query($conn, $quantity_query);
+							}
+
+							//Delete
+							function delete($conn, $sale_id, $isbn) {
+								$SQL = "DELETE FROM SALE WHERE sale_id='$sale_id' AND ISBN='$isbn'";
 								mysqli_query($conn, $SQL);
 							}
 
-							//TODO: delete not working
-							function delete($conn, $sale_id, $isbn) {
-								$SQL = "DELETE FROM SALE WHERE sale_id=\".$sale_id.\" AND ISBN=\".$isbn.\"";
-								mysqli_query($conn, $SQL);
-							}
 
 							$sql_query = "SELECT * FROM SALE NATURAL JOIN BOOK WHERE sold=0"; 
 							$mysqli_result = $conn->query($sql_query); 
-
+							
 
 							if(mysqli_num_rows($mysqli_result) > 0 ){
 								while($row = mysqli_fetch_array($mysqli_result)) {
-									echo "<table align=\"center\" BORDER=\"2\" CELLPADDING=\"2\" CELLSPACING=\"2\" WIDTH=\"100%\">";
-									echo "<th width='10%'>Remove</th><th width='60%'>Book Description</th><th width='10%'>Qty</th><th width='10%'>Price</th>";
-									echo "<tr><td><button name='delete' id='delete' onClick='del(" . $row['ISBN'] . ");return false;'>Delete Item</button></td>";
-									echo "<td>" . $row['title'] . "</br><b>By</b> " . $row['author'] . "</br><b>Publisher:</b> " . $row['publisher'] . "</td><td><input id='txt" . $row['ISBN'] . "' name='txt" . $row['ISBN'] . "' value='" . $row['quantity'] . "' size='1' /></td><td>" . $row['price'] . "</td>";
-									echo "</tr>					</table>";
+									$isbn = $row['ISBN'];
+									$title = $row['title'];
+									$author = $row['author'];
+									$publisher = $row['publisher'];
+									$quantity = $row['quantity'];
+									$price = $row['price'];
+									$sale_id = $row['sale_id'];
 
+								
+									echo "<tr><td><button name='delete' id='delete' onClick='del(" . $isbn . ");return false;'>Delete Item</button></td>";
+									echo "<td>" . $title . "</br><b>By</b> " . $author . "</br><b>Publisher:</b> " . $publisher . "</td>";
+									echo "<td><input id='" . $sale_id . "' name='" . $isbn . "' value='" . $quantity . "' size='1' onchange='url(" . $sale_id . ", " . $quantity . ", " . $isbn . ");'/></td>";
+									echo "<td>" . $price . "</td>";
+									echo "</tr>";
+									// echo "<p>" . $sale_id . "</p>";
+									
+									if (isset($_GET['delIsbn'])) {
+										$isbn = $_GET['delIsbn'];
+										delete($conn, $sale_id, $isbn);
+									}
 
-
-									//TODO
+									//TODO: if quantity changed, update db
 									if (isset($_GET['sale_id'])) {
 										$sale_id = $_GET['sale_id'];
 										$quantity = $_GET['quantity'];
 										$isbn = $_GET['isbn'];
-
 										urlFun($conn, $sale_id, $quantity, $isbn);
 									}
-
-									if (isset($_GET['sale_id'])) {
-										$sale_id = $_GET['sale_id'];
-										$isbn = $_GET['isbn'];
-
-										delete($conn, $sale_id, $isbn);
-									}
-
 								}
 							}
-							
-
-							
 						?>
+						</table>
 				</div>
 			</td>
 		</tr>
 		<tr>
 			<td align="center">				
-					<!-- TODO -->
-					<?php
-						echo "<input type='submit' name='recalculate_payment' id='recalculate_payment' value='Recalculate Payment' onClick='url(4, 2, 72227885);'>";
-					?>
+				<input type='submit' name='recalculate_payment' id='recalculate_payment' value='Recalculate Payment'>
 				</form>
 			</td>
 			<td align="center">
 				&nbsp;
 			</td>
-			<td align="center">		<!-- TODO -->	
-				Subtotal:  $12.99			</td>
+			<td align="center">
+				<?php
+					//Calculate; for each book: subtotal += quantity*price
+					$sql_query = "SELECT * FROM SALE NATURAL JOIN BOOK WHERE sold=0"; 
+					$mysqli_result = $conn->query($sql_query); 
+					
+					$sum = 0;
+					if(mysqli_num_rows($mysqli_result) > 0 ){
+						while($row = mysqli_fetch_array($mysqli_result)) {
+							$quantity = $row['quantity'];
+							$price = $row['price'];
+
+							$sum += $quantity * $price;
+						}
+					}
+					echo "Subtotal: $" . $sum;
+
+					$conn->close();
+				?>
+
+			</td>
 		</tr>
 	</table>
-	<?php
-		$conn->close();
-	?>
+
 </body>
