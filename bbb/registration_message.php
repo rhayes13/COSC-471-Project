a9149655a5a16b236fef117f40fbc8d9a05025d3
Sam <sgrone@emich.edu>
2020-12-03 17:04:39 -0800

    added message received when a user chooses not to register

---
+++ bbb/registration_message.php
@@ -0,0 +1,20 @@
+
+<head>
+<title>MESSAGE - 3-B.com</title>
+</head>
+<body>
+	<table align="center" style="border:1px solid blue;">
+		<tr>
+			<td style ="padding:10px" colspan="1" align="center">
+				<b>In order to proceed with the payment, you <br>need to register first.</b><br>
+			</td>
+		</tr>
+		<tr>
+			<td  align="center">
+				<form action="screen2.php" method="post">
+					<input type="submit" value="Manage Shopping Cart">
+				</form>
+			</td>
+		</tr>	
+	</table>
+</body>
